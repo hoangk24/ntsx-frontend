602a596ec352c611079d4cbb02bbb18b270f617d
nguyentamhoang <hoangtn4@gmail.com>
2022-05-17 17:39:23 +0700

    deploy app

--- src/components/paypal/PaypalProvider.tsx
+++ src/components/paypal/PaypalProvider.tsx
@@ -10,11 +10,7 @@ type Props = {
 export default function PaypalProvider({ children }: Props) {
  const options = {
   "client-id":
-   import.meta.env.VITE_CLIENT_ID ||
    "AbpVYNIhifd1sGCRXREus6oanzHHMNAVaR9BLXZFSSO9HCHitOvEQnxLsieZbGKDqKC1f71ms-7yY2rQ",
-  "data-client-token":
-   import.meta.env.VITE_DATA_CLIENT_TOKEN ||
-   "EHf1oHpizPuslQ6fQKC_BfBliqdKLUuMQaFtjHT49omr9UZw27jSRw2c-ORpvc4F4rihZfdW13evFbxb",
  };
  return (
   <PayPalScriptProvider options={options}>
